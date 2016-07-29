080470af6b393df93d4ee4f874a8119807fc8e65
David Axelrod <daxaxelrod@gmail.com>
2016-07-29 03:56:02 -0400

    everything works on the home page now

--- chooseASide/urls.py
+++ chooseASide/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name="all_topics"),
-    url(r"^(?P<topic>[\w\'-]+)/your-angle", views.create_angle, name="create_angle"),  # allow user to voice opinion
-    url(r"^(?P<topic>[\w\'-]+)/", views.topic, name="topic"),  # individual topic
+    url(r"^(?P<topic>[\w\'%-]+)/your-angle", views.create_angle, name="create_angle"),  # allow user to voice opinion
+    url(r"^(?P<topic>[\w\'%-]+)/", views.topic, name="topic"),  # individual topic
     # url(r'^(?P<topic>[\w-]+)/all_angles')
 ]
